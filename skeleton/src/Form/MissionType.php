f5675e5a8b73c6c3a6e55f591d5a3253e7e4c7a2
El Mahjoub <mahjoubwel@gmail.com>
2019-10-12 07:26:08 +0200

    mission controller

---
+++ skeleton/src/Form/MissionType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Mission;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MissionType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom')
+            ->add('description')
+            ->add('dateCreation')
+            ->add('date')
+            ->add('token')
+            ->add('user')
+            ->add('competences')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Mission::class,
+        ]);
+    }
+}
