8098c0b7a87f9b52ea7a0a00c7a33daba468d2c1
fitz35 <50512858+fitz35@users.noreply.github.com>
2019-10-12 06:06:45 +0200

    update implémenter et tester

--- skeleton/src/Controller/UserController.php
+++ skeleton/src/Controller/UserController.php
@@ -65,11 +65,25 @@ class UserController extends FOSRestController
     }
 
     /**
-     * @Rest\Delete("/user/update/{token}")
+     * @Rest\Put("/user/update")
      *
      * @return Response
      */
     public function updateUserAction(Request $request){
+        $data = json_decode($request->getContent(), true);
+        $token = $data["token"]; //$request ->attributes ->get("token");
+        $repository = $this->getDoctrine()->getRepository(User::class);
+        $user = $repository->findOneBy(['token' => $token]);
 
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->submit($data);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+            return $this->handleView($this->view(['status' => 'ok'], Response::HTTP_OK));
+        }
+        return $this->handleView($this->view($form->getErrors()));
     }
 }
